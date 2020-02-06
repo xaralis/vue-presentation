4937d6add13d53f05de83df96c98a8d59ca9683f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-06 13:44:51 +0000

    Deploy xaralis/vue-presentation to github.com/xaralis/vue-presentation.git:gh-pages

--- build/assetsPath.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const config = require('../config');
-
-module.exports = function(_path) {
-  return path.posix.join(config.assetsSubDirectory, _path)
-};
--- src/js/chart/memory/index.js
+++
@@ -1,5 +0,0 @@
-import './creating-clearing';
-import './ready-memory';
-import './replace-rows';
-import './run-memory';
-import './update-eatch';
