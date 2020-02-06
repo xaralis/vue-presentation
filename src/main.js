002a8930723cd1a3746836d72fba3a830c80c4cd
xaralis <filip.varecha@fragaria.cz>
2020-02-06 13:23:19 +0100

    Drop performance, not really that interesting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import reveal from 'reveal.js';
 import highlight from 'highlight.js';
 import './js/chart/stars-history';
-import './js/chart/benchmark';
-import './js/chart/memory';
 
 reveal.initialize({
   history: true,
